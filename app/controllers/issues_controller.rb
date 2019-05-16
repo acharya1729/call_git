49f587bb9a3ea7e99d8a2609af0a1c85fbad4fe9
acharya1729 <vijaya.mmmut@gmail.com>
2019-05-17 01:47:51 +0530

    rubocop fixes

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
+# Issues page, /issues/action_name, pass user and repo in params
 class IssuesController < ApplicationController
   def index
-    from, to = nil, nil
     user = params[:user]
     repo = params[:repo]
+    raise ActionController::ParameterMissing if user.nil? || repo.nil?
     git = GitIssue.new
     @response = git.issues(user: user, repo: repo)
     render status: :ok, json: @response
   end
-
 end
--- app/services/git_issue.rb
+++ app/services/git_issue.rb
@@ -1,31 +1,33 @@
+# frozen_string_literal: true
+
+# Git Issue uses the github_api gem
+# date_range_issue is a general method for any date range
 class GitIssue
+  def issues(user:, repo:)
+    issues = Github::Client::Issues.new
+    issues_list = issues.list(user: user, repo: repo, state: 'open').body
+    [
+      { open_today: todays_issue_list(issues_list: issues_list).count },
+      { open_this_week: weekly_issue_list(issues_list: issues_list).count },
+      { open_before_this_week: old_issues_list(issues_list: issues_list).count },
+      { totals_issues: issues_list.count }
+    ]
+  end
 
-	def issues(user:, repo:)
-		issues = Github::Client::Issues.new
-		issues_list = issues.list(user: user, repo: repo, state: 'open').body
-		[
-			{open_today: todays_issue_list(issues_list: issues_list).count},
-			{open_this_week: weekly_issue_list(issues_list: issues_list).count},
-			{open_before_this_week: old_issues_list(issues_list: issues_list).count},
-			{totals_issues: issues_list.count}
-		]					
-	end
+  def date_range_issue(issues_list:, from:, to:)
+    issues_list.select { |issue| issue.created_at >= from && issue.created_at <= to }.map(&:body)
+  end
 
-	def date_range_issue(issues_list:, from:, to:)
-		issues_list.select{|issue| issue.created_at >= from && issue.created_at <= to}.map(&:body)
-	end
+  def todays_issue_list(issues_list:)
+    issues_list.select { |issue| issue.created_at >= 1.day.ago }.map(&:body)
+  end
 
-	def todays_issue_list(issues_list:)
-		issues_list.select{|issue| issue.created_at >= 1.day.ago}.map(&:body)
-	end
+  def weekly_issue_list(issues_list:)
+    issues_list.select { |issue| issue.created_at >= 7.days.ago && issue.created_at <= 1.day.ago }
+               .map(&:body)
+  end
 
-	def weekly_issue_list(issues_list:)
-		issues_list.select{|issue| issue.created_at >= 7.days.ago && issue.created_at <= 1.day.ago}
-			.map(&:body)	
-	end
-
-	def old_issues_list(issues_list:)
-		issues_list.select{|issue| issue.created_at <= 7.days.ago}.map(&:body)
-	end
-
+  def old_issues_list(issues_list:)
+    issues_list.select { |issue| issue.created_at <= 7.days.ago }.map(&:body)
+  end
 end
